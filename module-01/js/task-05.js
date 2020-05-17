16852f728b6f685c03056f9fbfbc7e9338ed75c6
MellYu <mel6273569@gmail.com>
2020-05-17 13:44:22 +0300

    fixed cansel for task-5

--- module-01/js/task-05.js
+++ module-01/js/task-05.js
@@ -1,39 +1,47 @@
-let country = prompt('Введите страну доставки!');
+let country = prompt(`Введите страну доставки!`);
 let cost = 100;
-let result = (`Доставка в ${country} будет стоить ${cost} кредитов!`);
-let unCorrect = (`В вашей стране доставка не доступна`);
+let result = `Доставка в ${country} будет стоить ${cost} кредитов!`;
+let unCorrect = `В вашей стране доставка не доступна`;
 
-
-switch(country.toLowerCase()) {
-    case 'китай':
-        alert (result);
-        console.log(result);
-        break;
-    case 'чили':
-        cost = 250;
-        result = (`Доставка в ${country} будет стоить ${cost} кредитов!`);
-        alert (result);
-        console.log(result);
-        break; 
-    case 'австралия':
-        cost = 170;
-        result = (`Доставка в ${country} будет стоить ${cost} кредитов!`);
-        alert (result);
-        console.log(result);
-        break;   
-    case 'индия':
-        cost = 80;
-        result = (`Доставка в ${country} будет стоить ${cost} кредитов!`);
-        alert (result);
-        console.log(result);
-        break;
-    case 'ямайка':
-        cost = 120;
-        result = (`Доставка в ${country} будет стоить ${cost} кредитов!`);
-        alert (result);
-        console.log(result);
-        break;
+if (country === null) {
+  result = "Отменено пользователем!";
+  console.log(result);
+} else {
+  switch (country.toLowerCase()) {
+    case "китай":
+      alert(result);
+      console.log(result);
+      break;
+    case "чили":
+      cost = 250;
+      result = `Доставка в ${country} будет стоить ${cost} кредитов!`;
+      alert(result);
+      console.log(result);
+      break;
+    case "австралия":
+      cost = 170;
+      result = `Доставка в ${country} будет стоить ${cost} кредитов!`;
+      alert(result);
+      console.log(result);
+      break;
+    case "индия":
+      cost = 80;
+      result = `Доставка в ${country} будет стоить ${cost} кредитов!`;
+      alert(result);
+      console.log(result);
+      break;
+    case "ямайка":
+      cost = 120;
+      result = `Доставка в ${country} будет стоить ${cost} кредитов!`;
+      alert(result);
+      console.log(result);
+      break;
     default:
-        alert (unCorrect);
-        break;
+      alert(unCorrect);
+      break;
+    // case null:
+    //     result = ('Отменено пользователем!');
+    //     console.log(result);
+    //     break;
+  }
 }
