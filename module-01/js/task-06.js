ccd608cec6b5fd62ca21da3c97a572f953032637
MellYu <mel6273569@gmail.com>
2020-05-20 14:53:54 +0300

    putted const instead let

--- module-01/js/task-06.js
+++ module-01/js/task-06.js
@@ -1,15 +1,15 @@
 let total = 0;
 let input;
 
-do{
-    input = prompt('Введите число');
-    input =Number(input);
-    if(Number.isNaN(+input)){
+do {
+  input = prompt("Введите число");
+  input = Number(input);
+  if (Number.isNaN(+input)) {
     alert(`Было введено не число, попробуйте еще раз`);
     continue;
   }
 
-total += input;
-} while(input !== 0);
+  total += input;
+} while (input !== 0);
 
 console.log(`Общая сумма равна ${total}`);
