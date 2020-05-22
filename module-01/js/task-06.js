090b0ed9f4fa6f4af8585b812b22cd56eb8f9b87
MellYu <mel6273569@gmail.com>
2020-05-22 08:53:26 +0300

    fixed numbers in task-6

--- module-01/js/task-06.js
+++ module-01/js/task-06.js
@@ -3,13 +3,12 @@ let input;
 
 do {
   input = prompt("Введите число");
-  input = Number(input);
   if (Number.isNaN(+input)) {
     alert(`Было введено не число, попробуйте еще раз`);
     continue;
   }
 
-  total += input;
-} while (input !== 0);
+  total += +input;
+} while (input !== null);
 
 console.log(`Общая сумма равна ${total}`);
