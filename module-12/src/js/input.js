f2e5200a23c840a1978c05be7e2f3a6e65a829ea
MellYu <mel6273569@gmail.com>
2020-08-25 13:07:15 +0300

    fixed search on submit

--- module-12/src/js/input.js
+++ module-12/src/js/input.js
@@ -5,8 +5,7 @@ export default function handleCountyInput(event) {
   refs.countriesListRef.textContent = '';
   refs.countryContainerRef.textContent = '';
   const searchQuery = event.target.value;
-  if (searchQuery.length === 0) {
-    return;
+  if (searchQuery.length !== 0) {
+    fetchCountries(searchQuery);  
   }
-  fetchCountries(searchQuery);
 }
